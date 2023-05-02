aebff03f25f46f71e29a3956f31ac4752c7364f0
Gibran AP <90657980+Gibran-x1@users.noreply.github.com>
2023-05-02 16:55:37 -0600

    Add files via upload

--- segmentacion.py
+++ segmentacion.py
@@ -192,7 +192,7 @@ def video():
         ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
         # Aplicar la transformación de apertura
-        kernel = np.ones((3,3), np.uint8)
+        kernel = np.ones((1,1), np.uint8)
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 2)
 
         # Aplicar la transformación de fondo
